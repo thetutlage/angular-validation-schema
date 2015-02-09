60dbbe0907bb8e570a16cc9f57f49d2af95e24ad
Harminder Virk <virk.officials@gmail.com>
2015-02-09 23:07:37 +0530

    Added support for globals

--- angular-validation-schema.js
+++ angular-validation-schema.js
@@ -47,17 +47,10 @@
 				var defaultSchema = {
 					// default validation is set to required
 					'validations':'required',
-					// Messages to display
-					'messages':{
-						// on required
-						'required':{
-							'error': 'Required',
-							'success': 'Good Required'
-						}
-					},
-					// When to validate , default to watch
 					'validate-on':'watch'
 				};
+
+				var globalMessages = {};
 
 				// Grabbing schema the user wants
 				var schema = validationSchema.get(tAttrs.schema);
@@ -84,6 +77,22 @@
 				}
 
 				function setFixtures(schema){
+
+					// If schema has globals
+					if(schema.globals){
+
+						// Remove defaultSchema validations with globals
+						defaultSchema.validations = schema.globals.validations || defaultSchema.validations;
+
+						// Remove validate-on validations with globals
+						defaultSchema["validate-on"] = schema.globals["validate-on"] || defaultSchema["validate-on"];
+
+						// Set globalMessages if globals have one or empty object
+						globalMessages = schema.globals.messages || {};
+
+						// Delete schema globals as not required anymore
+						delete schema.globals;
+					}
 
 					// Deep extending objects
 					function extendDeep(dst) {
@@ -140,14 +149,24 @@
 							// Setting valid-method on field
 							i.attr('valid-method',i_schema['validate-on']);
 
+							// Extends messages using global and field values
+							i_schema.messages = i_schema.messages || {};
+							i_schema.messages = extendDeep(i_schema.messages,globalMessages);
+
 							// Looping through messages object
 							angular.forEach(i_schema.messages,function(vals,key){
 
+								// Grabbing error message and replace %field% with input name
+								var error_message = vals.error ? vals.error.replace('%field%',input_name) : '';
+
+								// Grabbing success message and replace %field% with input name
+								var success_message = vals.success ? vals.success.replace('%field%',input_name) : '';
+
 								// Setting error message for validation type
-								i.attr(key+'-error-message',vals.error || '');
+								i.attr(key+'-error-message',error_message);
 
 								// Setting success message for validation type
-								i.attr(key+'-success-message',vals.success || '');
+								i.attr(key+'-success-message',success_message);
 
 							});
 						}
